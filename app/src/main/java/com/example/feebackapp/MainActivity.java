62492afe92b39283d06e44e858cdbe3db6b875bc
kuku000 <kyle20010113@gmail.com>
2024-03-23 14:39:56 +0800

    Add the function of collecting input text and feedback to thelayout(by java)

--- app/src/main/java/com/example/feebackapp/MainActivity.java
+++ app/src/main/java/com/example/feebackapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.feebackapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button update_Button;
+    private EditText edittext;
+    private TextView data_textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        /*super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //MainCode
+        ById();//綁定元件
+        UpdateData();//上傳資料到TextView
+    }
+
+    public void ById(){
+        update_Button = findViewById(R.id.button);
+        edittext = findViewById(R.id.edittext);
+        data_textView = findViewById(R.id.text);
+    }
+    public void UpdateData(){
+        update_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = String.valueOf(edittext.getText());//拿取EditText字串
+                edittext.setText(""); //清除輸入框的資料
+                str = "Connect to "+str;
+                data_textView.setText(str);//設定文字到TextView
+            }
         });
+
     }
 }
